c2f2af3a5fa80e1ff7deb9e93293611b38da49f8
bilalamran2010-alt <bilal.amran2010@gmail.com>
2026-07-05 12:43:19 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -165,6 +165,5 @@ def verify():
     return jsonify({"success": False, "status": "limit", "message": "limit_reached"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080, threaded=True)
     
